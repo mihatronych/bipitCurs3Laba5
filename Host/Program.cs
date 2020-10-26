b8212c417a3b3fb71ce2b3974c08c41370278518
mihatronych <dolgushin.mikhail131200@yandex.ru>
2020-10-26 21:17:47 +0500

    There is the code!
    Код лабок с локалки

---
+++ Host/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfServiceLibrary;
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------WCF хост------");
+            using (ServiceHost serviceHost = new ServiceHost(typeof(Service1)))
+            {
+                serviceHost.Open();
+
+                Console.WriteLine("Сервис запущен...");
+                Console.WriteLine("Нажать Enter для остановки...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
